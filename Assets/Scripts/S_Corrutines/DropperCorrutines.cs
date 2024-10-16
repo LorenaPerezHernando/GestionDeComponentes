c91cdf1ee1ac8cf22b276e5833251a45dcf5d9a1
Lorena P <lorena.1997.lore@gmail.com>
2024-10-16 17:20:36 +0200

    Corrutines Fin

--- Assets/Scripts/S_Corrutines/DropperCorrutines.cs
+++ Assets/Scripts/S_Corrutines/DropperCorrutines.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DropperCorrutines : MonoBehaviour
 {
     //Attributes
-    public float repeatRate = 1;
     public GameObject ballPrefabs; 
     void Start()
     {
+        float repeatRate = Random.Range(0.5f, 3f);
         StartCoroutine(Drop(repeatRate));
     }
 
@@ -18,8 +18,16 @@ public class DropperCorrutines : MonoBehaviour
         
     }
 
-    IEnumerator Drop(float Rate)
+    IEnumerator Drop(float rateTime)
     {
-        yield return null; //Espera de un frame 
+
+        while (true)
+        {
+            //yield return null; //Espera de un frame 
+            //yield return rateTime; //Espera el tiempo de spawn
+
+            yield return new WaitForSeconds(rateTime);
+            GameObject ball = Instantiate(ballPrefabs, transform.position, Quaternion.identity);
+        }
     }
 }
